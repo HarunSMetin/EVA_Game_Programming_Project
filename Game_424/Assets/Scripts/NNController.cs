65e7500ed0c0e1b8af8d1ff71dafd133e57e72ae
hpekkan <hpekkan@etu.edu.tr>
2023-08-03 09:50:43 +0300

    Neural network prefabs added Neural network initilization script completed

--- Game_424/Assets/Scripts/NNController.cs
+++ Game_424/Assets/Scripts/NNController.cs
@@ -1,32 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class NNController : MonoBehaviour
 {
 
     public GameObject NeuralNetwork;
-    public GameObject[][] Layers;
-    
-    
+    public GameObject[][][] Layers ;
+    public Dictionary<GameObject, int[]> ActivePerceptrons = new Dictionary<GameObject, int[]>();
+    public GameObject inputLayer;
+    public GameObject outputLayer;
+    public GameObject[][] inputPerceptrons;
+    public GameObject[][] outputPerceptrons;
+
     void Start()
     {
-        NeuralNetwork = GameObject.FindWithTag("NeuralNetwork");
-        Layers = new GameObject[NeuralNetwork.transform.childCount][];
-        for (int i = 0; i < NeuralNetwork.transform.childCount; i++)
+        //Hidden Layer
+        NeuralNetwork = GameObject.FindWithTag("HiddenLayer");
+        GameObject[] layers = GameObject.FindGameObjectsWithTag("Layers");
+        Layers = new GameObject[layers.Length][][];
+        for (int x =0;x<layers.Length;x++)
         {
-            Layers[i] = new GameObject[NeuralNetwork.transform.GetChild(i).childCount];
-            for (int j = 0; j < NeuralNetwork.transform.GetChild(i).childCount; j++)
+            Layers[x] = new GameObject[layers[x].transform.childCount][];
+            for(int y = 0; y < layers[x].transform.childCount; y++)
             {
-                Layers[i][j] = NeuralNetwork.transform.GetChild(i).GetChild(j).gameObject;
+                Layers[x][y] = new GameObject[layers[x].transform.GetChild(y).childCount];
+                GameObject row = layers[y].transform.GetChild(y).gameObject;
+                for(int z = 0; z < row.transform.childCount; z++)
+                {
+                    GameObject perceptron = row.transform.GetChild(z).gameObject;
+                    Layers[x][y][z] = perceptron;
+                }
             }
         }
-        Debug.Log(Layers[0]);
-    }
+        
+        //Input Layer
+        inputLayer = GameObject.FindWithTag("InputLayer");
+        inputPerceptrons = new GameObject[inputLayer.transform.childCount][];
+        for(int i =0;i<inputLayer.transform.childCount;i++)
+        {
+            GameObject row = inputLayer.transform.GetChild(i).gameObject;
+            inputPerceptrons[i] = new GameObject[row.transform.childCount];
+            for(int j = 0; j < row.transform.childCount; j++)
+            {
+                GameObject perceptron = row.transform.GetChild(j).gameObject;
+                inputPerceptrons[i][j] = perceptron;
+            }
+        }
 
-    
+        //Output Layer
+        outputLayer = GameObject.FindWithTag("OutputLayer");
+        outputPerceptrons = new GameObject[outputLayer.transform.childCount][];
+        for (int i = 0; i < outputLayer.transform.childCount; i++)
+        {
+            GameObject row = outputLayer.transform.GetChild(i).gameObject;
+            outputPerceptrons[i] = new GameObject[row.transform.childCount];
+            for (int j = 0; j < row.transform.childCount; j++)
+            {
+                GameObject perceptron = row.transform.GetChild(j).gameObject;
+                outputPerceptrons[i][j] = perceptron;
+            }
+        }
+        Debug.Log("Layers: " + Layers.Length);
+        Debug.Log("Input Layer: " + inputLayer.transform.childCount);
+        Debug.Log("Output Layer: " + outputLayer.transform.childCount);
+
+        
+}
+
+
     void Update()
     {
-        
+       
     }
 }
