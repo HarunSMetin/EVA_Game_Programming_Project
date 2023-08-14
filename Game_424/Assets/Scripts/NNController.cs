ffc9b30506fbcbb4397864d18d5d6b2613981b65
hpekkan <hpekkan@etu.edu.tr>
2023-08-14 19:05:42 +0300

    Working state of NN; feature: Audio Effects Added; feature: corrupted Neuron added

--- Game_424/Assets/Scripts/NNController.cs
+++ Game_424/Assets/Scripts/NNController.cs
@@ -7,48 +7,69 @@ public class NNController : MonoBehaviour
 {
 
     public GameObject NeuralNetwork;
-    public GameObject[][][] Layers ;
-    public Dictionary<GameObject, int[]> ActivePerceptrons = new Dictionary<GameObject, int[]>();
+    public GameObject[][][] Layers;
+    public Dictionary<GameObject, float> ActivePerceptrons = new Dictionary<GameObject, float>();
     public GameObject inputLayer;
     public GameObject outputLayer;
     public GameObject[][] inputPerceptrons;
     public GameObject[][] outputPerceptrons;
-
-    void Start()
+    public int corruptedX=2, corruptedY=1;
+    private GameObject blinkingPerceptron;
+    private Material originalMaterial;
+    public float blinkingDuration = 0.5f;
+    public Material glowingMaterial;
+    public Material corruptedMaterial;
+    public GameObject corruptedPerceptron;
+    private int inputLayerSizeX, inputLayerSizeY, outputLayerSizeX, outputLayerSizeY, hiddenLayerSizeX, hiddenLayerSizeY, hiddenLayerSizeZ;
+    public AudioSource AudioSource;
+    void Awake()
     {
         //Hidden Layer
         NeuralNetwork = GameObject.FindWithTag("HiddenLayer");
         GameObject[] layers = GameObject.FindGameObjectsWithTag("Layers");
         Layers = new GameObject[layers.Length][][];
-        for (int x =0;x<layers.Length;x++)
+
+        for (int x = 0; x < layers.Length; x++)
         {
             Layers[x] = new GameObject[layers[x].transform.childCount][];
-            for(int y = 0; y < layers[x].transform.childCount; y++)
+
+            for (int y = 0; y < layers[x].transform.childCount; y++)
             {
-                Layers[x][y] = new GameObject[layers[x].transform.GetChild(y).childCount];
-                GameObject row = layers[y].transform.GetChild(y).gameObject;
-                for(int z = 0; z < row.transform.childCount; z++)
+                Transform row = layers[x].transform.GetChild(y);
+                Layers[x][y] = new GameObject[row.childCount];
+
+                for (int z = 0; z < row.childCount; z++)
                 {
-                    GameObject perceptron = row.transform.GetChild(z).gameObject;
+                    GameObject perceptron = row.GetChild(z).gameObject;
                     Layers[x][y][z] = perceptron;
+                    perceptron.name = "Hidden Perceptron "+z +" "+y+" "+x;
                 }
             }
         }
         
+
+        hiddenLayerSizeZ = Layers.Length;
+        hiddenLayerSizeY = Layers[0].Length;
+        hiddenLayerSizeX = Layers[0][0].Length;
         //Input Layer
         inputLayer = GameObject.FindWithTag("InputLayer");
         inputPerceptrons = new GameObject[inputLayer.transform.childCount][];
-        for(int i =0;i<inputLayer.transform.childCount;i++)
+        for (int i = 0; i < inputLayer.transform.childCount; i++)
         {
             GameObject row = inputLayer.transform.GetChild(i).gameObject;
             inputPerceptrons[i] = new GameObject[row.transform.childCount];
-            for(int j = 0; j < row.transform.childCount; j++)
+            for (int j = 0; j < row.transform.childCount; j++)
             {
                 GameObject perceptron = row.transform.GetChild(j).gameObject;
                 inputPerceptrons[i][j] = perceptron;
+                perceptron.name = "Input Perceptron " +i+" "+j;
             }
         }
-
+        inputLayerSizeX = inputPerceptrons.Length;
+        inputLayerSizeY = inputPerceptrons[0].Length;
+        corruptedPerceptron = inputPerceptrons[corruptedX][corruptedY];
+        Material corruptedNeuron = corruptedPerceptron.GetComponent<Renderer>().material;
+        corruptedNeuron  = corruptedMaterial;
         //Output Layer
         outputLayer = GameObject.FindWithTag("OutputLayer");
         outputPerceptrons = new GameObject[outputLayer.transform.childCount][];
@@ -60,18 +81,102 @@ public class NNController : MonoBehaviour
             {
                 GameObject perceptron = row.transform.GetChild(j).gameObject;
                 outputPerceptrons[i][j] = perceptron;
+                perceptron.name = "Output Perceptron " +i+" "+j;
             }
         }
-        Debug.Log("Layers: " + Layers.Length);
-        Debug.Log("Input Layer: " + inputLayer.transform.childCount);
-        Debug.Log("Output Layer: " + outputLayer.transform.childCount);
+        outputLayerSizeX = outputPerceptrons.Length;
+        outputLayerSizeY = outputPerceptrons[0].Length;
 
-        
-}
+
+    }
 
 
     void Update()
     {
-       
+        //loop active perceptrons
+        foreach (KeyValuePair<GameObject, float> entry in ActivePerceptrons.ToList())
+        {
+            if (entry.Value >= blinkingDuration)
+            {
+                Renderer rendererLocal = entry.Key.GetComponent<Renderer>();
+                rendererLocal.material = (GameObject.ReferenceEquals(entry.Key,corruptedPerceptron))?corruptedMaterial:originalMaterial;
+                
+                ActivePerceptrons.Remove(entry.Key);
+            }
+            else if (entry.Value < blinkingDuration)
+            {
+                ActivePerceptrons[entry.Key] = entry.Value + Time.deltaTime;
+            }
+
+        }
+
+
+        /*if (isBlinking && Time.time >= blinkEndTime)
+        {
+            Renderer renderer = blinkingPerceptron.GetComponent<Renderer>();
+            renderer.material = originalMaterial;
+            isBlinking = false;
+        }*/
+        Material selectedMaterial = glowingMaterial;
+        // Blink input perceptron
+        if (ActivePerceptrons.Count == 0)
+        {
+            int randomInputX = Random.Range(0, inputLayerSizeX);
+            int randomInputY = Random.Range(0, inputLayerSizeY);
+            Debug.Log("Random Input X: " + randomInputX + " Random Input Y: " + randomInputY+" Corrupted X: "+corruptedX+" Corrupted Y: "+corruptedY);
+            if (randomInputX == corruptedX && randomInputY == corruptedY)
+            {
+                Debug.Log("here");
+                selectedMaterial = corruptedMaterial;
+                AudioSource.Play();
+            }
+            else
+            {
+                selectedMaterial = glowingMaterial;
+            }
+            blinkingPerceptron = inputPerceptrons[randomInputX][randomInputY];
+            ActivePerceptrons.Add(blinkingPerceptron, 0.0f);
+            originalMaterial = blinkingPerceptron.GetComponent<Renderer>().material;
+
+            Renderer renderer = blinkingPerceptron.GetComponent<Renderer>();
+            renderer.material = glowingMaterial; // Apply the glowing material
+            renderer.material.EnableKeyword("_EMISSION"); // Enable emission
+
+
+
+            // Blink perceptrons in hidden layers
+            for (int z = 0; z < hiddenLayerSizeZ; z++)
+            {
+                
+                int randomX = Random.Range(0, hiddenLayerSizeX);
+                int randomY = Random.Range(0, hiddenLayerSizeY);
+                
+                blinkingPerceptron = Layers[z][randomY][randomX];
+                ActivePerceptrons.Add(blinkingPerceptron, 0.0f);
+
+
+                originalMaterial = blinkingPerceptron.GetComponent<Renderer>().material;
+
+                Renderer rendererHidden = blinkingPerceptron.GetComponent<Renderer>();
+                rendererHidden.material = glowingMaterial; // Apply the glowing material
+
+
+            }
+
+
+            // Blink perceptron in output layer
+            int randomOutputX = Random.Range(0, outputLayerSizeX);
+            int randomOutputY = Random.Range(0, outputLayerSizeY);
+            blinkingPerceptron = outputPerceptrons[randomOutputX][randomOutputY];
+            ActivePerceptrons.Add(blinkingPerceptron, 0.0f);
+            originalMaterial = blinkingPerceptron.GetComponent<Renderer>().material;
+
+            Renderer rendererOut = blinkingPerceptron.GetComponent<Renderer>();
+            rendererOut.material = glowingMaterial; // Apply the glowing material
+            rendererOut.material.EnableKeyword("_EMISSION"); // Enable emission
+
+
+            // Increment the sequence index and wrap around if necessary
+        }
     }
 }
