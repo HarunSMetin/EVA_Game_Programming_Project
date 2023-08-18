bdc28ac47dd06eaab9d1737a54879cbf9e5f8e41
Berkay Yıldız <berkayyildiz550@hotmail.com>
2023-08-18 15:56:44 +0300

    last day changes and scripts prepared

--- Game_424/Assets/Scripts/BoxMovement.cs
+++ Game_424/Assets/Scripts/BoxMovement.cs
@@ -2,59 +2,81 @@ using UnityEngine;
 
 public class BoxMovement : MonoBehaviour
 {
-    public float interactionDistance = 2f; // Karakterin kutuya olan etkileþim mesafesi
-    private bool isHolding = false;
-    public GameObject interactableCube;
-    private Vector3 initialOffset;
+    public GameObject[] cubes;
+    public GameObject[] door;
+    public GameObject[] trueOrderCubes;
 
-    private void Update()
+    public void Start()
     {
-        // Raycast ile etkileþime geçilecek kutuyu tespit etme
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        trueOrderCubes = new GameObject[cubes.Length+1];
+        trueOrderCubes[0] = cubes[3];
+        trueOrderCubes[1] = cubes[2];
+        trueOrderCubes[2] = cubes[1];
+        trueOrderCubes[3] = cubes[0];
+    }
 
-        if (Physics.Raycast(ray, out hit, interactionDistance))
+    private void IsCubesTrueOrder()
+    {
+        //if boxes have true order, door[0]'s z index will be -3 and door[1]'s z index will be +3
+        if (cubes[0].transform.position == trueOrderCubes[0].transform.position &&
+                       cubes[1].transform.position == trueOrderCubes[1].transform.position &&
+                                  cubes[2].transform.position == trueOrderCubes[2].transform.position &&
+                                             cubes[3].transform.position == trueOrderCubes[3].transform.position)
         {
-            if (hit.collider.CompareTag("InteractableCube"))
-            {
-                interactableCube = hit.collider.gameObject;
-            }
-            else
-            {
-                interactableCube = null;
-            }
-        }
-        else
-        {
-            interactableCube = null;
-        }
-
-        // E tuþuna basýldýðýnda kutuyu taþýmak veya býrakmak
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            if (!isHolding && interactableCube != null)
-            {
-                isHolding = true;
-                initialOffset = interactableCube.transform.position - GetMouseWorldPosition();
-            }
-            else if (isHolding)
-            {
-                isHolding = false;
-            }
-        }
-
-        // Kutuyu taþýma iþlemi
-        if (isHolding && interactableCube != null)
-        {
-            Vector3 targetPosition = GetMouseWorldPosition() + initialOffset;
-            interactableCube.transform.position = new Vector3(targetPosition.x, targetPosition.y, interactableCube.transform.position.z);
+            Debug.Log("true order");
+            door[0].transform.position = new Vector3(door[0].transform.position.x, door[0].transform.position.y, door[0].transform.position.z - 3);
+            door[1].transform.position = new Vector3(door[1].transform.position.x, door[1].transform.position.y, door[0].transform.position.z + 3);
         }
     }
 
-    private Vector3 GetMouseWorldPosition()
+    //there are 4 cubes and 3 capsules. Capsules are in the middle of the cubes. If capsules[i] is pressed "E", cubes[i] and cubes[i+1] will be swapped
+    public void OnButtonPressed(int index)
     {
-        Vector3 mousePosition = Input.mousePosition;
-        mousePosition.z = Camera.main.WorldToScreenPoint(interactableCube.transform.position).z;
-        return Camera.main.ScreenToWorldPoint(mousePosition);
+        Debug.Log("xx " + index + " pressed");
+        if (index == 0)
+        {
+            Debug.Log("aaa " + index + " pressed");
+            //if capsules[0] is pressed "E", cubes[0] and cubes[1] will be swapped
+            Vector3 temp = cubes[0].transform.position;
+            cubes[0].transform.position = cubes[1].transform.position;
+            cubes[1].transform.position = temp;
+            GameObject temp2 = cubes[0];
+            cubes[0] = cubes[1];
+            cubes[1] = temp2;
+        }
+        else if (index == 1)
+        {
+            //if capsules[1] is pressed "E", cubes[1] and cubes[2] will be swapped
+            Vector3 temp = cubes[1].transform.position;
+            cubes[1].transform.position = cubes[2].transform.position;
+            cubes[2].transform.position = temp;
+            GameObject temp2 = cubes[1];
+            cubes[1] = cubes[2];
+            cubes[2] = temp2;
+        }
+        else if (index == 2)
+        {
+            //if capsules[2] is pressed "E", cubes[2] and cubes[3] will be swapped
+            Vector3 temp = cubes[2].transform.position;
+            cubes[2].transform.position = cubes[3].transform.position;
+            cubes[3].transform.position = temp;
+            GameObject temp2 = cubes[2];
+            cubes[2] = cubes[3];
+            cubes[3] = temp2;
+        }
+        
+        IsCubesTrueOrder();
     }
+
+    //how to understand which phere is pressed "E"? 
+    //1. Create a sphere and add a script to it.
+    //2. Add a tag to the sphere.
+    //3. Add a collider to the sphere.
+    //4. Add a rigidbody to the sphere.
+    //5. Add a script to the sphere.
+    // what script do I add to the sphere?
+    // public class ButtonOnClick : MonoBehaviour
+    // {
+    //     //If sphere pressed "E", call boxMovement.OnButtonPressed(i)
+
 }
